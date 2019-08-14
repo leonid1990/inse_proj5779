6bae7758e020956335ae4dc5472af1d73a69d60d
unknown <leonid1990@gmail.com>
2019-08-14 22:35:38 +0300

    added auth route and controller

---
+++ backend/routes/api/auth.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+const controller = require("../../conrollers/auth");
+
+// @route  GET api/auth
+// @desc   Test route
+// @access Public
+router.get("/", (req, res) => {
+  res.send("Auth route");
+});
+
+module.exports = router;
