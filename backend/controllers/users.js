dd271ec39d67c8f5308d6bd517dceeab42eea5fb
unknown <leonid1990@gmail.com>
2019-08-18 04:56:57 +0300

    added jwt signature for signup

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,7 +1,9 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
+const config = require("config");
 
 exports.signup = async (req, res) => {
   const errors = validationResult(req);
@@ -24,9 +26,24 @@ exports.signup = async (req, res) => {
 
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
+
     await user.save();
 
-    res.send("User registered");
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      config.get("jwtSecret"),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
