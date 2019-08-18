829fdf831de60a38280e8ec1253b0cb3339954ab
unknown <leonid1990@gmail.com>
2019-08-18 17:04:31 +0300

    finished authentication with jwt

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,17 +1,26 @@
 var express = require("express");
+const { check } = require("express-validator");
 var router = express.Router();
 
 const authController = require("../../controllers/auth");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 
-router.get("/", auth, authController.getUser);
-
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-// router.get("/", (req, res) => {
-//   res.send("Auth route");
-// });
+router.get("/", auth, authController.testAuth);
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  authController.authenticateUser
+);
 
 module.exports = router;
