1b3a622e69e9f134fc8fa4524abe51786518977e
unknown <leonid1990@gmail.com>
2019-08-18 16:36:43 +0300

    added jwt verification middleware

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
-const controller = require("../../controllers/auth");
+const authController = require("../../controllers/auth");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+
+router.get("/", auth, authController.getUser);
 
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => {
-  res.send("Auth route");
-});
+// router.get("/", (req, res) => {
+//   res.send("Auth route");
+// });
 
 module.exports = router;
