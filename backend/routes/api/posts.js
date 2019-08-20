b44a798fb6672353e16f78285e1c84945888dc7c
unknown <leonid1990@gmail.com>
2019-08-20 17:39:32 +0300

    added post creation

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -1,13 +1,31 @@
 var express = require("express");
+const { check } = require("express-validator");
 var router = express.Router();
 
-const postsController = require("../../controllers/posts");
+const postController = require("../../controllers/posts");
+const auth = require("../../middleware/auth");
+
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  postController.createPost
+);
 
 // @route  GET api/posts
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => {
-  res.send("Posts route");
-});
+// router.get("/", (req, res) => {
+//   res.send("Posts route");
+// });
 
 module.exports = router;
