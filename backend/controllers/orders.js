ed31eab38978c26eb2cbe8aa82d4d9dd545cca07
unknown <leonid1990@gmail.com>
2019-08-23 13:17:10 +0300

    added orders route and order creation

---
+++ backend/controllers/orders.js
@@ -0,0 +1,35 @@
+const { validationResult } = require("express-validator");
+
+const Order = require("../models/Order");
+const Product = require("../models/Product");
+const User = require("../models/User");
+
+exports.createOrder = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    for (const item of req.body.details) {
+      await Product.findById(item.product, (err, product) => {
+        item.title = product.title;
+      });
+    }
+
+    const newOrder = new Order({
+      user: req.body.user,
+      name: user.name,
+      details: req.body.details,
+      address: req.body.address
+    });
+
+    const order = await newOrder.save();
+
+    res.json("order");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+};
---
+++ backend/routes/api/orders.js
@@ -0,0 +1,36 @@
+var express = require("express");
+const { check } = require("express-validator");
+var router = express.Router();
+
+const orderController = require("../../controllers/orders");
+const auth = require("../../middleware/auth");
+
+// @route    POST api/orders
+// @desc     Create a order
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("address", "address is required")
+        .not()
+        .isEmpty(),
+      check(
+        "details.*.product",
+        "array of products and their quantities is required"
+      )
+        .not()
+        .isEmpty(),
+      check(
+        "details.*.quantity",
+        "array of products and their quantities is required"
+      )
+        .not()
+        .isEmpty()
+    ]
+  ],
+  orderController.createOrder
+);
+
+module.exports = router;
